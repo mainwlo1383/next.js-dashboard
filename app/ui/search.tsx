bf1b6bb03f3c012bd203691968ff5279471be1a2
minashih <mina_shih@pxmart.com.tw>
2024-04-11 17:42:05 +0800

    feat. ch12

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,19 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter  } from 'next/navigation';
-
+import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ placeholder }: { placeholder: string }) {
- const searchParams = useSearchParams();
+// /dashboard/invoices?page=1&query=pending would look like this: 
+// {page: '1', query: 'pending'}
+ const searchParams = useSearchParams(); 
  const pathname = usePathname();
  const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams); 
+    params.set('page', '1');
+    
     if (term) {
       params.set('query', term);
     } else {
@@ -17,7 +22,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } 
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+ 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
